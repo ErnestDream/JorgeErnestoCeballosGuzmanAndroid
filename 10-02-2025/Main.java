927b8b3bf389d8be640bbdc8bf036e4eefe78f5d
ErnestDream <ernestdreemurr@gmail.com>
2025-02-11 19:49:36 -0600

    Calculadora funcional con inputs

--- 10-02-2025/Main.java
+++ 10-02-2025/Main.java
@@ -1,31 +1,34 @@
 import java.io.*;
+import java.util.Scanner;
 
 class Main{
 
 	public static void main(String [] args) {
 
-		Operacion op = new Operacion(10.0, 10.0, Operando.SUMA);
+		Scanner scanner = new Scanner(System.in);
+
+		//scanner de las variables
+		System.out.println("Introduce el primer numero");
+		double num1 = scanner.nextDouble();
+		System.out.println("Introduce el segundo numero");
+		double num2 = scanner.nextDouble();
 		
+		//Scanner de la operacion a realizar
+		System.out.println("Introduce la operacion a realizar SUMA, RESTA, MULT, DIV");
+		String strOperacion = scanner.next().toUpperCase();
+		Operando operacion = Operando.valueOf(strOperacion);
+
+		//Crear la operacion
+		Operacion op = new Operacion(num1, num2, operacion);
+
+		//Crear la calculadora y calcular el resultado
 		ICalculadora calculadora = new CalculadoraImpl();
-
 		Double resultado = calculadora.calcular(op);
-		System.out.println("El resultado de la suma es " + resultado);
 
-		//resta
-		op = new Operacion(10.0, 10.0, Operando.RESTA);
-                resultado = calculadora.calcular(op);
-                System.out.println("El resultado de la resta es " + resultado);
-
-		//Multiplicacion
-                op = new Operacion(10.0, 10.0, Operando.MULT);
-                resultado = calculadora.calcular(op);
-                System.out.println("El resultado de la multiplicacion es " + resultado);
-
-		//Division
-                op = new Operacion(10.0, 10.0, Operando.DIV);
-                resultado = calculadora.calcular(op);
-                System.out.println("El resultado de la division es " + resultado);
-
-
+		//Mostrar el resultado
+		System.out.println("El resultado de la operacion es: " + resultado);
+		
+		//Cerrar el scanner
+		scanner.close();
 	}
 }
